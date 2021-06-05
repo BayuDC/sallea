a5708bffab2042e06bccccca52921a9c9a8831cd
Bayu <revalinastevani@gmail.com>
2021-06-05 23:04:34 +0700

    preparation

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/default');
+app.use(morgan('dev'));
+app.use(express.static('./public'));
+app.use(expressLayouts);
+
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.render('index');
 });
 
 app.listen(port, () => console.log('Server is running...'));
