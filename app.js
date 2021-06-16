834f426e93f39c1bf2b45998fa703048ac9ecac1
Bayu <revalinastevani@gmail.com>
2021-06-16 10:04:28 +0700

    final

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const sharp = require('sharp');
 const path = require('path');
 const waifu = require('./utils/waifu');
 const appLocals = require('./app.locals');
-const { host, port, nsfw } = require('./config.json');
+const { nsfw } = require('./config.json');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.locals = { ...appLocals, nsfw: nsfw.enable };
 app.set('view engine', 'ejs');
@@ -35,7 +36,6 @@ app.get('/nsfw/(:tag?)', async (req, res, next) => {
 app.get(['/(:tag?)', '/nsfw/(:tag?)'], (req, res, next) => {
     const type = req.query.type;
     const images = res.locals.images;
-    images.length = 2;
     if (type && type == 'json') {
         if (!images) return res.sendStatus(404);
         return res.status(200).json(images);
@@ -74,4 +74,4 @@ app.get('/d/(:name)', async (req, res) => {
 app.use((req, res) => {
     res.status(404).render('error');
 });
-app.listen(port, () => console.log(`Server is running at ${host}:${port}`));
+app.listen(port, () => console.log(`Listening at port ${port}`));
